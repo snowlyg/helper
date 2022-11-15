206c5c3768abb40fa4451f51d5a0a1da6f4b1eb4
snowlyg <569616226@qq.com>
2022-11-15 08:32:03 +0800

    add cookie support

--- http/http.go
+++ http/http.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"strings"
 	"time"
@@ -12,14 +13,16 @@ import (
 
 type client struct {
 	Config *Config
+	Cookie *http.Cookie
 }
 
 type Config struct {
-	TimeOver int64
-	TimeOut  int64
-	Account  string            // 账号
-	Pwd      string            // 密码
-	Headers  map[string]string // request headers
+	TimeOver   int64
+	TimeOut    int64
+	Account    string            // 账号
+	Pwd        string            // 密码
+	Headers    map[string]string // request headers
+	CookieName string
 }
 
 func NewClient(config *Config) *client {
@@ -36,16 +39,18 @@ type ServerResponse struct {
 	Path     string      `json:"path"`
 	BaseAuth bool        `json:"baseAuth"`
 	Data     interface{} `json:"data"`
+	Body     io.Reader
+	Fields   map[string]string
 }
 
-//Post  提交数据
+// Post  提交数据
 func (n *client) Post(sr *ServerResponse, data string) ([]byte, error) {
-	result := n.request("POST", sr.Path, data, sr.BaseAuth)
+	result := n.request("POST", sr.Path, sr.BaseAuth, strings.NewReader(data))
 	if len(result) == 0 {
 		return result, fmt.Errorf("Post %s 没有返回数据", sr.Path)
 	}
 	if !json.Valid(result) {
-		return result, nil
+		return result, fmt.Errorf("result is not valid")
 	}
 	err := json.Unmarshal(result, sr.Data)
 	if err != nil {
@@ -54,23 +59,67 @@ func (n *client) Post(sr *ServerResponse, data string) ([]byte, error) {
 	return result, nil
 }
 
-//GetFile  下载文件
+// GetFile  下载文件
 func (n *client) GetFile(sr *ServerResponse) ([]byte, error) {
-	result := n.request("GET", sr.Path, "", sr.BaseAuth)
-	if len(result) == 0 {
-		return result, fmt.Errorf("Get %s 没有返回数据", sr.Path)
-	}
-	return result, nil
-}
-
-//Get  获取数据
-func (n *client) Get(sr *ServerResponse) ([]byte, error) {
-	result := n.request("GET", sr.Path, "", sr.BaseAuth)
+	result := n.request("GET", sr.Path, sr.BaseAuth, nil)
 	if len(result) == 0 {
 		return result, fmt.Errorf("Get %s 没有返回数据", sr.Path)
 	}
 	if !json.Valid(result) {
-		return result, nil
+		return result, fmt.Errorf("result is not valid")
+	}
+	err := json.Unmarshal(result, sr.Data)
+	if err != nil {
+		return result, fmt.Errorf("执行解码失败: %s 错误：%w ,结果: %v", sr.Path, err, string(result))
+	}
+	return result, nil
+}
+
+// Upload  上传文件
+func (n *client) Upload(sr *ServerResponse) ([]byte, error) {
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	fw, err := writer.CreateFormFile("file", sr.Fields["filename"])
+	if err != nil {
+		return nil, fmt.Errorf("CreateFormFile %v", err)
+	}
+
+	_, err = io.Copy(fw, sr.Body)
+	if err != nil {
+		return nil, fmt.Errorf("copying fileWriter %v", err)
+	}
+
+	for k, v := range sr.Fields {
+		_ = writer.WriteField(k, v)
+	}
+	err = writer.Close() // close writer before POST request
+	if err != nil {
+		return nil, fmt.Errorf("writerClose: %v", err)
+	}
+	n.Config.Headers["Content-Type"] = writer.FormDataContentType()
+	result := n.request("POST", sr.Path, sr.BaseAuth, body)
+	if len(result) == 0 {
+		return result, fmt.Errorf("Get %s 没有返回数据", sr.Path)
+	}
+	if !json.Valid(result) {
+		return result, fmt.Errorf("result is not valid")
+	}
+	err = json.Unmarshal(result, sr.Data)
+	if err != nil {
+		return result, fmt.Errorf("执行解码失败: %s 错误：%w ,结果: %v", sr.Path, err, string(result))
+	}
+	return result, nil
+}
+
+// Get  获取数据
+func (n *client) Get(sr *ServerResponse) ([]byte, error) {
+
+	result := n.request("GET", sr.Path, sr.BaseAuth, nil)
+	if len(result) == 0 {
+		return result, fmt.Errorf("Get %s 没有返回数据", sr.Path)
+	}
+	if !json.Valid(result) {
+		return result, fmt.Errorf("result is not valid")
 	}
 	err := json.Unmarshal(result, sr.Data)
 	if err != nil {
@@ -80,22 +129,24 @@ func (n *client) Get(sr *ServerResponse) ([]byte, error) {
 	return result, nil
 }
 
-func (n *client) request(method, url, data string, basicAuth bool) []byte {
+func (n *client) request(method, url string, basicAuth bool, body io.Reader) []byte {
 	result := make(chan []byte, 30)
 	T := time.NewTicker(time.Duration(n.Config.TimeOver) * time.Second)
 	go func() {
 		t := time.Duration(n.Config.TimeOut) * time.Second
 		Client := http.Client{Timeout: t}
-		req, err := http.NewRequest(method, url, strings.NewReader(data))
+		req, err := http.NewRequest(method, url, body)
 		if err != nil {
 			result <- nil
 			return
 		}
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+
 		if len(n.Config.Headers) > 0 {
 			for key, value := range n.Config.Headers {
 				req.Header.Set(key, value)
 			}
+		} else {
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
 		}
 		if basicAuth {
 			req.SetBasicAuth(n.Config.Account, n.Config.Pwd)
@@ -105,6 +156,13 @@ func (n *client) request(method, url, data string, basicAuth bool) []byte {
 		if err != nil {
 			result <- nil
 			return
+		}
+		if n.Config.CookieName != "" && resp.Cookies() != nil {
+			for _, cookie := range resp.Cookies() {
+				if cookie.Name == n.Config.CookieName {
+					n.Cookie = cookie
+				}
+			}
 		}
 		defer resp.Body.Close()
 
